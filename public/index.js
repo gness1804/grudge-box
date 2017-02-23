57a37564e1de552063068179e58ca329b36158f2
gness1804 <graham.nessler@gmail.com>
2017-02-23 12:01:47 -0700

    Enemy's name now comes onto the page on load from app.locals

--- public/index.js
+++ public/index.js
@@ -2,6 +2,34 @@ const enemyNameInput = $('#enemy-name-input')
 const enemyOffenseInput = $('#enemy-offense-input')
 const enemyDateInput = $('#enemy-date-input')
 const addEnemyButton = $('#add-enemy-button')
+const enemyListDisplay = $('.enemy-list-display')
+
+const putEnemiesOnPage = (enemies) => {
+  enemyListDisplay.html('')
+  enemies.forEach((enemy) => {
+    enemyListDisplay.append(`
+      <div>
+        <h2>${enemy.name}</h2>
+      </div>
+      `)
+  });
+}
+
+const getEnemies = () => {
+  const hitAPI = new XMLHttpRequest();
+  hitAPI.open('GET', '/api/vi/enemies', true);
+  hitAPI.send();
+  hitAPI.onreadystatechange = function () {
+    if (hitAPI.readyState === XMLHttpRequest.DONE) {
+      if (hitAPI.status === 200) {
+        const result = JSON.parse(hitAPI.responseText)
+        putEnemiesOnPage(result)
+      } else {
+        console.error('There was a problem with the API call.'); // eslint-disable-line
+      }
+    }
+  }
+}
 
 const dataIsValid = (name, offense, date) => {
   if (typeof name === 'string' && typeof offense === 'string' && typeof date === 'string' && name && offense && date) {
@@ -23,5 +51,8 @@ addEnemyButton.on('click', () => {
     name,
     offense,
     date,
+    forgiven: false,
   })
 })
+
+getEnemies()
--- server.js
+++ server.js
@@ -14,7 +14,15 @@ app.set('port', process.env.PORT || 3000);
 
 app.locals.title = 'Grudge Box'
 
-app.locals.enemies = []
+app.locals.enemies = [
+  {
+    id: 372921,
+    name: 'The Grinch',
+    offense: 'Stealing Christmas',
+    date: '12/25/2016',
+    forgiven: false,
+  },
+]
 
 app.get('/', (request, response) => {
   response.sendFile(path.join(__dirname, 'public/index.html'));
@@ -28,7 +36,7 @@ app.listen(app.get('port'), () => {
   console.log('The server is listening on port 3000.'); // eslint-disable-line
 });
 
-// app.post('/api/vi/enemies', (request, response) => {
-//   const enemyInfo = request.body
-//   app.locals.enemies.push(enemyInfo)
-// });
+app.post('/api/vi/enemies', (request, response) => {
+  const enemyInfo = request.body
+  app.locals.enemies.push(enemyInfo)
+});
