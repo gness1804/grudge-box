4240ae78b3ed1834f73df9e073ff8b04bd2d2d41
gness1804 <graham.nessler@gmail.com>
2017-02-23 17:54:51 -0700

    First feature test passes

---
+++ test/feature-tests.js
@@ -0,0 +1,25 @@
+/* eslint-disable */
+const assert = require('assert');
+const webdriver = require('selenium-webdriver');
+const test = require('selenium-webdriver/testing');
+
+const driver = new webdriver.Builder()
+  .forBrowser('chrome')
+  .build()
+
+test.describe('App', function () {
+
+  this.timeout(15000);
+
+  test.beforeEach(function() {
+      driver.get('http://localhost:3000');
+    });
+
+    test.it('application should serve up the index html when user visits it', function () {
+      driver.findElement({tagName: 'h1'}).then(function (title) {
+        return title.getText()
+      }).then(function (text) {
+        assert.strictEqual(text, 'Grudge Box');
+  })
+    })
+})
